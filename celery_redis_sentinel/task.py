f933cab7616de53b4e13dc68329af38964f12024
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2016-01-14 17:13:15 -0500

    added docs for EnsuredRedisTask [ci skip]

--- celery_redis_sentinel/task.py
+++ celery_redis_sentinel/task.py
@@ -7,6 +7,23 @@ from .redis_sentinel import ensure_redis_call
 
 
 class EnsuredRedisTask(Task):
+    """
+    Abstract celery task subclass which provides same functionality as
+    :py:class:`ensure_redis_call <celery_redis_sentinel.redis_sentinel.ensure_redis_call>`
+    except it is added at task definition time instead of during task schedule call.
+
+    This task subclass can be provided during task definition
+    by using ``base`` parameter.
+
+    Examples
+    --------
+
+    ::
+
+        @app.task(base=EnsuredRedisTask)
+        def add(a, b):
+            return a + b
+    """
     abstract = True
 
     def apply_async(self, *args, **kwargs):
